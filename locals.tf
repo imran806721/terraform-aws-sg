888848e7f734f558c50940ce985682baa3ebe9e8
imran <imran806721@gmail.com>
2026-09-07 17:46:53 +0530

    excuted

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals{
+    common_name = "${var.project}-${var.environment}-${var.sg_name}"
+    common_tags = {
+        Project = var.project
+        Environment = var.environment
+        Terraform = "true"
+    }
+}
